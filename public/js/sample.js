2deb2dfaa6d3d801ca2ca2069539f6a1264c3a1e
junpeizuno <junpeizuno@gmail.com>
2021-03-30 14:25:15 +0900

    title 画面でenterkeyを取得 => focusを article 画面へ

--- public/js/sample.js
+++ public/js/sample.js
@@ -1,5 +1,15 @@
-document.getElementById('textarea1').focus();
+//writeページにアクセスした際にフォーカスをtextareaに合わせる。
+document.getElementById('textarea').focus();
 
+
+//textareaに文字を入力した際に横幅いっぱいになると自動で改行する。
+let textarea = document.getElementById('textarea');
+let clientHeight = textarea.clientHeight;
+textarea.addEventListener('input', ()=>{
+    textarea.style.height = clientHeight + 'px';
+    let scrollHeight = textarea.scrollHeight;
+    textarea.style.height = scrollHeight + 'px';
+});
 
 let textarea1 = document.getElementById('textarea1');
 let clientHeight1 = textarea1.clientHeight;
@@ -9,14 +19,10 @@ textarea1.addEventListener('input', ()=>{
     textarea1.style.height = scrollHeight1 + 'px';
 });
 
-
-
-
-
-let textarea = document.getElementById('textarea');
-let clientHeight = textarea.clientHeight;
-textarea.addEventListener('input', ()=>{
-    textarea.style.height = clientHeight + 'px';
-    let scrollHeight = textarea.scrollHeight;
-    textarea.style.height = scrollHeight + 'px';
-});
+//title 画面でenter　key を取得した際にキキーコード取得 => enter => focusを article 画面へ
+document.getElementById('textarea').onkeypress = (e) => {
+    const key = e.keyCode || e.charCode || 13;
+    if(key == 13){
+        document.getElementById('textarea1').focus();
+    }
+}
